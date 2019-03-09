f0c793094a3ae19b4303e32527962187459c3312
dillon <dmanhall@gmail.com>
2019-03-09 00:00:18 -0700

    added more headers for functions from the git repo in the LTE board.  Gonna just work on usart from now on.

--- main.c
+++ main.c
@@ -14,7 +14,9 @@
 
 
 
-void setup();
+void setup(); 
+void serialSetup();
+void i2cSetup();
 
 void main(void) 
 {
@@ -29,9 +31,29 @@ void main(void)
     return;
 }
 
+/*
+ * setup is the genral boot up functions for the pic micro
+ * this sets up the clock, interupts and the digial io stuff  
+ * Every thing else is handled by their respective functions
+ * 
+ * rember boys, making a tris bit true is an input, false is an output
+ * 
+ */
+
 void setup()
 {
-    OSCCON=0b01101100;    
+    OSCCON=0b01101100; // configure the internal clock
     OPTION_REG=0b10001111;
     INTCON=0b10100000; //we may not need this
+    // for the time being make port A an input and port c and output 
+    TRISC = 0x00;
+    TRISA = 0xFF;
+    
+    serialSetup(); // call the function for setting up the serial devices 
+}
+
+// sets up the stuff 
+void serialSetup()
+{
+    
 }
