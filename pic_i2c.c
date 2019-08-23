8f4ab0b4dff5dac99b1a0a351d5a86b1cc111080
dillon <dillon@dillondesktop>
2019-08-23 01:23:51 -0600

    progress on function to write a byte out to an i2c client

--- pic_i2c.c
+++ pic_i2c.c
@@ -1,23 +1,5 @@
 #include "pic_i2c.h"
 
-void i2c_wait()
-{
-    while(R_nW || (SSPCON2 & 0x1F));
-    return;
-}
-
-char i2c_sendbyte(I2C_DEVICE_T *dev,char *data,unsigned char datalen)
-{
-    
-    return 0; // zero on success
-}
-
-char i2c_readbyte(I2C_DEVICE_T *dev,char *data,unsigned char datalen)
-{
-    
-    return -1; // stub, when success return the number of bytes read from device
-}
-
 I2C_DEVICE_T new_device(unsigned char addr)
 {
     I2C_DEVICE_T nd;
@@ -26,3 +8,31 @@ I2C_DEVICE_T new_device(unsigned char addr)
     nd.isConnected = false;
     return nd;
 }
+
+void i2c_wait()
+{
+    while(R_nW || (SSPCON2 & 0x1F));
+    return;
+}
+
+char i2c_sendbyte(I2C_DEVICE_T *dev,char *data,unsigned char datalen)
+{
+    unsigned char writeaddr = dev->i2c_addr & ~(1 << 0); // bit zero should be clear for writting 
+    while(R_nW || (SSPCON2 & 0x1F));
+    SEN = 1; // start condition
+    while(R_nW || (SSPCON2 & 0x1F));
+    SSPBUF = writeaddr; // after the start condition is out send out the write addr
+    while(R_nW || (SSPCON2 & 0x1F));
+    SSPBUF = *data; // data to write out
+    while(R_nW || (SSPCON2 & 0x1F));
+    PEN = 1; // stop condition
+    
+    return 0; 
+}
+
+char i2c_readbyte(I2C_DEVICE_T *dev,char *data,unsigned char datalen)
+{
+    
+    return -1; // stub, when success return the number of bytes read from device
+}
+
